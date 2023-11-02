f982384af56803343a07259b887cdae47cff5706
StephenAdeosun <stephenadeosun411@gmail.com>
2023-11-02 22:03:07 +0100

    grt-started oage

--- components/get-started/FormDetails.tsx
+++ components/get-started/FormDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { millik,mukta, openSans } from "../../app/font";
 import Link from 'next/link'
+import { ArrowLeft} from 'iconsax-react'
 function FormDetails() {
   return (
     <div className={`max-w-[552px] shadow-md border-[1px] rounded-[20px] p-[20px] mt-[30px] m-auto`}>
@@ -24,12 +25,12 @@ function FormDetails() {
   </div>
 
   <div className='flex flex-col'>
-    <label htmlFor="name" className={`text-[#6C7480] text-[12px] ${openSans.className} font-[600]`}>Company Name</label>
+    <label htmlFor="name" className={`text-[#6C7480] text-[12px] ${openSans.className} font-[600]`}>Industry</label>
     <input type="text" placeholder='Type Here' className={`border-[1px] placeholder:text-[16px] placeholder:font-[400] py-[16px] px-[8px] border-[#E5E5E5] bg-[#F8F8F8] rounded-[5px]  mt-[10px] ${mukta.className} text-[14px]`} />
   </div>
 
   <div className='flex flex-col'>
-    <label htmlFor="name" className={`text-[#6C7480] text-[12px] ${openSans.className} font-[600]`}>Company Name</label>
+    <label htmlFor="name" className={`text-[#6C7480] text-[12px] ${openSans.className} font-[600]`}>Country Region</label>
     <input type="text" placeholder='Type Here' className={`border-[1px] placeholder:text-[16px] placeholder:font-[400] py-[16px] px-[8px] border-[#E5E5E5] bg-[#F8F8F8] rounded-[5px]  mt-[10px] ${mukta.className} text-[14px]`} />
   </div>
 
@@ -53,17 +54,20 @@ function FormDetails() {
     <input type="text" placeholder='Tell us a bit about the app you are interested in building...' className={`border-[1px] placeholder:text-[16px] placeholder:font-[400] py-[16px] px-[8px] border-[#E5E5E5] bg-[#F8F8F8] rounded-[5px]  mt-[10px] ${mukta.className} text-[14px]`} />
   </div>
 
-<Link href='/'>
-  <h1>Back</h1>
+<div className='flex justify-end items-center gap-[16px]' >
+<Link href='/' className='items-center gap-[1px] flex'><ArrowLeft size={16} />
+  <h1 className={`text-[#3B4350] font-[700] text-[18px] inline ${mukta.className}`}>
+     Back </h1>
 </Link>
-<button type="submit">Next</button>
-
+<button type="submit" className={`px-[20px] py-[16px] rounded-[40px] w-[114px] text-[#FFF] bg-[#D0E1FF] ${mukta.className} text-[16px] font-[700]`}>Next</button>
+</div>
 </form>
 </div>
 
     </div>
   )
 }
+
 
 
 export default FormDetails
