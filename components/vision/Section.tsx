d9fa68e0258747ee43e6e68f0002e0e979e92195
StephenAdeosun <stephenadeosun411@gmail.com>
2023-11-27 14:08:56 +0100

    new

--- components/vision/Section.tsx
+++ components/vision/Section.tsx
@@ -5,7 +5,7 @@ function Section() {
   return (
     <div className={`max-w-[1152px] mt-[116px] m-auto bg-[#0A2656] rounded-[20px] `}>
         <div className={` font-[500]  max-w-[1104px] md:text-[24px] text-[18px] p-[24px]  text-[#FFFFFF] ${mukta.className} text-center `}>
-        We do this because we want to help business scale fast, develop novelty solutions and solve unique business problem. If you can dream it, we can build it.
+        We do this because we want to help businesses scale fast, develop novelty solutions and solve unique business problem. If you can dream it, we can build it.
         </div>
     </div>
   )
